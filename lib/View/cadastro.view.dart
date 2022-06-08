86ab63736b9a2e1cdf57a5c7861b4fef28efabd6
Vitor-Kemuel <kemuvitor@gmail.com>
2022-06-08 18:11:02 -0300

    Adicionando form na tela de cadastro

--- lib/View/cadastro.view.dart
+++ lib/View/cadastro.view.dart
@@ -14,202 +14,206 @@ class _CadastroViewState extends State<CadastroView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      color: Color.fromARGB(255, 4, 0, 255),
-      child: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-        SizedBox(
-          height: 500,
-          width: 450,
-          child: Image.asset('images/logo_japas_food.jpeg'),
-        ),
-        Container(
-          margin: EdgeInsets.only(
-            top: 4,
-            right: displayWidth() / 20,
-            left: displayWidth() / 20,
-          ),
-          height: 50,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(50),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 3,
-                )
-              ]),
-          child: TextField(
-            keyboardType: TextInputType.name,
-            cursorColor: Colors.blue,
-            decoration: InputDecoration(
-              hintText: 'Nome Completo',
-              prefixIcon: Icon(
-                Icons.person,
+        body: Form(
+      child: Container(
+        color: const Color.fromARGB(255, 4, 0, 255),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 500,
+                width: 450,
+                child: Image.asset('images/logo_japas_food.jpeg'),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 4,
+                  right: displayWidth() / 20,
+                  left: displayWidth() / 20,
+                ),
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 3,
+                    ),
+                  ],
+                ),
+                child: const TextField(
+                  keyboardType: TextInputType.name,
+                  cursorColor: Colors.blue,
+                  decoration: InputDecoration(
+                    hintText: 'Nome Completo',
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: Colors.blue,
+                    ),
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 15,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 10,
+                  right: displayWidth() / 20,
+                  left: displayWidth() / 20,
+                ),
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 3,
+                    ),
+                  ],
+                ),
+                child: const TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  cursorColor: Colors.blue,
+                  decoration: InputDecoration(
+                    hintText: 'E-mail',
+                    prefixIcon: Icon(
+                      Icons.alternate_email,
+                      color: Colors.blue,
+                    ),
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 15,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 10,
+                  right: displayWidth() / 20,
+                  left: displayWidth() / 20,
+                ),
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 3,
+                    ),
+                  ],
+                ),
+                child: const TextField(
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  cursorColor: Colors.blue,
+                  decoration: InputDecoration(
+                    hintText: 'Senha',
+                    prefixIcon: Icon(
+                      Icons.vpn_key,
+                      color: Colors.blue,
+                    ),
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 15,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 10,
+                  right: displayWidth() / 20,
+                  left: displayWidth() / 20,
+                ),
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 3,
+                    ),
+                  ],
+                ),
+                child: const TextField(
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  cursorColor: Colors.blue,
+                  decoration: InputDecoration(
+                    hintText: 'Confirmar Senha',
+                    prefixIcon: Icon(
+                      Icons.vpn_key,
+                      color: Colors.blue,
+                    ),
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 15,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 10,
+                  right: displayWidth() / 20,
+                  left: displayWidth() / 20,
+                ),
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black26,
+                      blurRadius: 3,
+                    ),
+                  ],
+                ),
+                child: const TextField(
+                  keyboardType: TextInputType.phone,
+                  cursorColor: Colors.blue,
+                  decoration: InputDecoration(
+                    hintText: 'Telefone Ex: (17) 98888-8888',
+                    prefixIcon: Icon(
+                      Icons.phone,
+                      color: Colors.blue,
+                    ),
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 15,
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                height: 50,
+                margin: EdgeInsets.symmetric(
+                  horizontal: displayWidth() / 20,
+                  vertical: 25,
+                ),
                 color: Colors.blue,
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Cadastrar-se',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
               ),
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 15,
-              ),
-            ),
+            ],
           ),
-        ),
-        Container(
-          margin: EdgeInsets.only(
-            top: 10,
-            right: displayWidth() / 20,
-            left: displayWidth() / 20,
-          ),
-          height: 50,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(50),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 3,
-                )
-              ]),
-          child: TextField(
-            keyboardType: TextInputType.emailAddress,
-            cursorColor: Colors.blue,
-            decoration: InputDecoration(
-              hintText: 'E-mail',
-              prefixIcon: Icon(
-                Icons.alternate_email,
-                color: Colors.blue,
-              ),
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 15,
-              ),
-            ),
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.only(
-            top: 10,
-            right: displayWidth() / 20,
-            left: displayWidth() / 20,
-          ),
-          height: 50,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(50),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 3,
-                )
-              ]),
-          child: TextField(
-            keyboardType: TextInputType.visiblePassword,
-            obscureText: true,
-            cursorColor: Colors.blue,
-            decoration: InputDecoration(
-              hintText: 'Senha',
-              prefixIcon: Icon(
-                Icons.vpn_key,
-                color: Colors.blue,
-              ),
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 15,
-              ),
-            ),
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.only(
-            top: 10,
-            right: displayWidth() / 20,
-            left: displayWidth() / 20,
-          ),
-          height: 50,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(50),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 3,
-                )
-              ]),
-          child: TextField(
-            keyboardType: TextInputType.visiblePassword,
-            obscureText: true,
-            cursorColor: Colors.blue,
-            decoration: InputDecoration(
-              hintText: 'Confirmar Senha',
-              prefixIcon: Icon(
-                Icons.vpn_key,
-                color: Colors.blue,
-              ),
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 15,
-              ),
-            ),
-          ),
-        ),
-        Container(
-          margin: EdgeInsets.only(
-            top: 10,
-            right: displayWidth() / 20,
-            left: displayWidth() / 20,
-          ),
-          height: 50,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(50),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black26,
-                  blurRadius: 3,
-                )
-              ]),
-          child: TextField(
-            keyboardType: TextInputType.phone,
-            cursorColor: Colors.blue,
-            decoration: InputDecoration(
-              hintText: 'Telefone Ex: (17) 98888-8888',
-              prefixIcon: Icon(
-                Icons.phone,
-                color: Colors.blue,
-              ),
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 15,
-              ),
-            ),
-          ),
-        ),
-        Container(
-          height: 50,
-          margin: EdgeInsets.symmetric(
-            horizontal: displayWidth() / 20,
-            vertical: 25,
-          ),
-          child: RaisedButton(
-            onPressed: () {},
-            color: Colors.blue,
-            child: Text(
-              'Cadastrar-se',
-              style: TextStyle(color: Colors.white),
-            ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(50),
-            ),
-          ),
-        ),
-          ],
         ),
       ),
     ));
