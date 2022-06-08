36483f9dd92373e6b8fd1c9621471e75aafd3014
Vitor-Kemuel <kemuvitor@gmail.com>
2022-06-08 18:10:21 -0300

    Corrigindo importação

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'view/app.dart';
+import 'View/app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
